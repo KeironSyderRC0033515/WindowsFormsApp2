80a9493e53fd0318c0150d17de6ea605724ad614
rc0033515 <rc0033515@CRO-24-01>
2018-09-18 09:59:19 +0100

    successful pathway

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -17,9 +17,32 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
+            for (int i = 0; i < 30; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X + 2, pictureBox1.Location.Y);
+                await Task.Delay(20);
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 2);
+                await Task.Delay(20);
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X - 2, pictureBox1.Location.Y);
+                await Task.Delay(20);
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 2);
+                await Task.Delay(20);
+            }
 
-        }
+
+
+
+            }
     }
 }
